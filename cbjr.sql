baa3edbcd9e5ae6da8ea7055bfd97358b105af44
MarceloSPTECH <marcelo.cruzjr@sptech.school>
2022-11-13 21:43:45 -0300

    adicionada a api do web-data-viz

---
+++ cbjr.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE cbjr;
+USE cbjr;
+
+CREATE TABLE usuario (
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nomeUsuario VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    senha VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE pontuacao (
+	idPontuacao INT,
+    fkUsuario INT, CONSTRAINT fkIdUsuario 
+		FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
+			PRIMARY KEY (idPontuacao, fkUsuario),
+	pontuacao INT,
+    perPont FLOAT
+);	
